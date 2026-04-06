74a31637b86fb14ba0a1a56e8695df6ce7ab210b
StephenNgo97 <stephenngo97@gmail.com>
2026-04-06 16:33:20 +1000

    Configured input to be stored as list of types and optional verbose output

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 from google import genai
+from google.genai import types
+import argparse
 
 load_dotenv()
 
@@ -10,15 +12,22 @@ def main():
     if api_key is None:
         raise RuntimeError("GEMINI_API_KEY is not available")
     client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt to be passed into the LLM")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     content = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
     if content.usage_metadata is None:
         raise RuntimeError("Request Failed")
     else:
-        print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
         print(content.text)
 
 if __name__ == "__main__":
