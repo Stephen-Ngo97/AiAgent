345529f819ea137d5b769ef646f41765653745ad
StephenNgo97 <stephenngo97@gmail.com>
2026-04-09 13:21:51 +1000

    Implemented LLM call loop so that agent can iteratively service requests

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 load_dotenv()
 
@@ -23,25 +23,50 @@ def main():
     args = parser.parse_args()
     # Create messages list with user prompt
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    # Pass in messages list to generate content
-    content = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)    
-    )
-    if content.usage_metadata is None:
+
+    # Call AI model in a loop with maximum of 20 iterations
+    for call_loop in range(20):
+        # Pass in messages list to generate content
+        content = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)    
+        )
+        # Add response if any to messages list
+        if content.candidates: 
+            for response in content.candidates:
+                messages.append(response.content)
         # Assume API request failed if usage metadata is not available
-        raise RuntimeError("Request Failed")
-    else:
-        # If verbose is enabled, print the user prompt, prompt tokens, and response tokens
-        if args.verbose:
-            print(f"User prompt: {args.user_prompt}")
-            print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
-        if content.function_calls is not None:
-            for function_call in content.function_calls:
-                print(f"Calling function: {function_call.name}({function_call.args})")
-        print(content.text)
+        if content.usage_metadata is None:  
+            raise RuntimeError("Request Failed")
+        else:
+            # If verbose is enabled, print the user prompt, prompt tokens, and response tokens
+            if args.verbose:
+                print(f"User prompt: {args.user_prompt}")
+                print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+            # Run all function calls if any
+            if content.function_calls is not None:
+                function_results = []
+                for function_call in content.function_calls:
+                    function_call_result = call_function(function_call, verbose=args.verbose)
+                    if not function_call_result.parts:
+                        raise Exception("Function call failed")
+                    if function_call_result.parts[0].function_response is None:
+                        raise Exception("Function call did not provide a response")
+                    if function_call_result.parts[0].function_response.response is None:
+                        raise Exception("Function call did not provide a response")
+                    function_results.append(function_call_result.parts[0])
+                    if args.verbose:
+                        print(f"-> {function_call_result.parts[0].function_response.response}")
+                # Append results of function calls to messages
+                messages.append(types.Content(role="user", parts=function_results))
+            else:     
+                print(content.text)
+                return None 
+    print("Maximum number of iterations reached - exiting program")
+    sys.exit(1)
+            
 
 
 if __name__ == "__main__":
